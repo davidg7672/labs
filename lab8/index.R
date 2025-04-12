e898b01100788ed4a6bda3d0e35179f861314e10
DavidSosa <davidgsosa1@gmail.com>
2025-04-11 20:11:31 -0700

    finished lab7

---
+++ lab8/index.R
@@ -0,0 +1,9 @@
+#### Lab 8: Alcohol Deaths and Fixed Effects ####
+setwd("")
+rm(list = ls())
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(stargazer)
+library(corrplot)
+library(janitor)
